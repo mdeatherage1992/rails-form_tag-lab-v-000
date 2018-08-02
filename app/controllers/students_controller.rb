9b553641628be0c5ca3d90b76deab62f4e7a1148
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-02 16:00:57 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,9 @@ class StudentsController < ApplicationController
   def new
   end
 
+  def create
+  Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+end
+
 end
